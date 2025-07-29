504957b090b8789e10aa087ee95d2e3fe533a5d2
eeser2 <54655082+eeser2@users.noreply.github.com>
2025-07-29 14:35:06 -0700

    Added Username and Password fields and the Log In Button

--- broom_service_app/app/login.tsx
+++ broom_service_app/app/login.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { useRouter } from 'expo-router';
 import { AntDesign } from '@expo/vector-icons';
 
@@ -7,11 +7,38 @@ export default function Login() {
 
   return (
     <View style={styles.container}>
+
+      {/* Back Button */}
       <TouchableOpacity style={styles.backButton} onPress={() => router.back()} >
-        {/* <Text style={styles.backButtonText} onPress={() => router.back()} >Back</Text> */}
         <AntDesign name="arrowleft" size={30} color={"#fff"} />
       </TouchableOpacity>
+
+      {/* Log In Title Text */}
       <Text style={styles.loginTitle}>Log In</Text>
+
+      {/* User Log in Fields */}
+      <View style={styles.userInputGroup}>
+        <Text style={styles.fieldLabel}>Username or Email</Text>
+        <TextInput 
+          style={styles.userInput}
+          placeholder="example@gmail.com"
+          placeholderTextColor="#aaa"
+          autoCapitalize="none"
+          keyboardType="email-address"
+        />
+        <Text style={styles.fieldLabel}>Password</Text>
+        <TextInput 
+          style={styles.userInput}
+          placeholder="*******"
+          placeholderTextColor="#aaa"
+          secureTextEntry
+        />
+      </View>
+
+      {/* Log In Button */}
+      <TouchableOpacity style={styles.loginButton} onPress={() => alert("Log in Button is Pressed.")} >
+        <Text style={styles.loginButtonText}>Log In</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -35,7 +62,19 @@ const styles = StyleSheet.create({
     fontFamily: "Cochin",
     fontWeight: "bold",
     marginTop: 150,
+    marginBottom: 70,
   }, 
+  userInputGroup: {
+    width: 300,
+    alignItems: 'flex-start',
+    marginBottom: 25,
+  },
+  fieldLabel: {
+    color: '#fff',
+    fontSize: 18,
+    marginBottom: 5,
+    fontWeight: "bold",
+  },
   userInput: {
     width: 300,
     height: 50,
@@ -43,6 +82,30 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     paddingHorizontal: 16,
     fontSize: 20,
-    marginBottom: 16,
+    marginBottom: 45,
+  },
+  loginButton: {
+    backgroundColor: '#4e9bde',
+    paddingVertical: 16,
+    paddingHorizontal: 40,
+    borderRadius: 10,
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  loginButtonText: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  forgotPassOrRegister: {
+    marginTop: 20,
+    alignItems: 'center',
+    gap: 10,
+  },
+  forgotPassOrRegisterText: {
+    color: '#4e9bde',
+    fontSize: 16,
+    textDecorationLine: 'underline',
+    marginTop: 5,
   },
 });
