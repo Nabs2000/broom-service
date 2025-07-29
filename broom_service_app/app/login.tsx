6fc727a8a8f1243279cf20f7d0a2d758985ccd52
eeser2 <54655082+eeser2@users.noreply.github.com>
2025-07-29 14:53:29 -0700

    Added a keyboard escape when tapping on areas outside the fields.

--- broom_service_app/app/login.tsx
+++ broom_service_app/app/login.tsx
@@ -1,45 +1,53 @@
-import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
-import { useRouter } from 'expo-router';
+import { Keyboard, StyleSheet, Text, TextInput, TouchableOpacity, TouchableWithoutFeedback, View } from 'react-native';
+import { Link, useRouter } from 'expo-router';
 import { AntDesign } from '@expo/vector-icons';
 
 export default function Login() {
   const router = useRouter();
 
   return (
-    <View style={styles.container}>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+      <View style={styles.container}>
 
-      {/* Back Button */}
-      <TouchableOpacity style={styles.backButton} onPress={() => router.back()} >
-        <AntDesign name="arrowleft" size={30} color={"#fff"} />
-      </TouchableOpacity>
+        {/* Back Button */}
+        <TouchableOpacity style={styles.backButton} onPress={() => router.back()} >
+          <AntDesign name="arrowleft" size={30} color={"#fff"} />
+        </TouchableOpacity>
 
-      {/* Log In Title Text */}
-      <Text style={styles.loginTitle}>Log In</Text>
+        {/* Log In Title Text */}
+        <Text style={styles.loginTitle}>Log In</Text>
 
-      {/* User Log in Fields */}
-      <View style={styles.userInputGroup}>
-        <Text style={styles.fieldLabel}>Username or Email</Text>
-        <TextInput 
-          style={styles.userInput}
-          placeholder="example@gmail.com"
-          placeholderTextColor="#aaa"
-          autoCapitalize="none"
-          keyboardType="email-address"
-        />
-        <Text style={styles.fieldLabel}>Password</Text>
-        <TextInput 
-          style={styles.userInput}
-          placeholder="*******"
-          placeholderTextColor="#aaa"
-          secureTextEntry
-        />
+        {/* User Log in Fields */}
+        <View style={styles.userInputGroup}>
+          <Text style={styles.fieldLabel}>Username or Email</Text>
+          <TextInput 
+            style={styles.userInput}
+            placeholder="example@gmail.com"
+            placeholderTextColor="#aaa"
+            autoCapitalize="none"
+            keyboardType="email-address"
+          />
+          <Text style={styles.fieldLabel}>Password</Text>
+          <TextInput 
+            style={styles.userInput}
+            placeholder="*******"
+            placeholderTextColor="#aaa"
+            secureTextEntry
+          />
+        </View>
+
+        {/* Log In Button */}
+        <TouchableOpacity style={styles.loginButton} onPress={() => alert("Log in Button is Pressed.")} >
+          <Text style={styles.loginButtonText}>Log In</Text>
+        </TouchableOpacity>
+
+        {/* Forgot Password or Register */}
+        <View style={styles.forgotPassOrRegister}>
+          <Text style={styles.forgotPassOrRegisterText} onPress={() => alert("Forgot Password is pressed.")}>Forgot Password? Reset</Text>
+          <Link href="/signup" style={styles.forgotPassOrRegisterText}>Don&apos;t have an account? Sign Up</Link>
+        </View>
       </View>
-
-      {/* Log In Button */}
-      <TouchableOpacity style={styles.loginButton} onPress={() => alert("Log in Button is Pressed.")} >
-        <Text style={styles.loginButtonText}>Log In</Text>
-      </TouchableOpacity>
-    </View>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -82,7 +90,7 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     paddingHorizontal: 16,
     fontSize: 20,
-    marginBottom: 45,
+    marginBottom: 3,
   },
   loginButton: {
     backgroundColor: '#4e9bde',
