136fd8f3a48ddcec9956afe49ca262c68d22a3fe
eeser2 <54655082+eeser2@users.noreply.github.com>
2025-07-29 13:59:18 -0700

    Added back button to the login page

--- broom_service_app/app/login.tsx
+++ broom_service_app/app/login.tsx
@@ -1,9 +1,17 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { useRouter } from 'expo-router';
+import { AntDesign } from '@expo/vector-icons';
 
 export default function Login() {
+  const router = useRouter();
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>This is the login page.</Text>
+      <TouchableOpacity style={styles.backButton} onPress={() => router.back()} >
+        {/* <Text style={styles.backButtonText} onPress={() => router.back()} >Back</Text> */}
+        <AntDesign name="arrowleft" size={30} color={"#fff"} />
+      </TouchableOpacity>
+      <Text style={styles.loginTitle}>Log In</Text>
     </View>
   );
 }
@@ -13,9 +21,28 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
-  text: {
+  backButton: {
+    position: 'absolute',
+    top: 80,
+    left: 20,
+    padding: 10,
+  },
+  loginTitle: {
     color: '#fff',
+    fontSize: 100,
+    fontFamily: "Cochin",
+    fontWeight: "bold",
+    marginTop: 150,
+  }, 
+  userInput: {
+    width: 300,
+    height: 50,
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    paddingHorizontal: 16,
+    fontSize: 20,
+    marginBottom: 16,
   },
 });
