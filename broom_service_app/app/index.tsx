54b07469f442a60edebcd1e9fc0b8d9a79a6f024
Nabeel Sabzwari <nabeelsabz101@gmail.com>
2025-07-29 13:18:49 -0700

    Add link to login page

--- broom_service_app/app/index.tsx
+++ broom_service_app/app/index.tsx
@@ -1,11 +1,19 @@
-import { StyleSheet, Text, View } from 'react-native';
-import { Link } from 'expo-router';
+import { StyleSheet, Text, View } from "react-native";
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>This is the landing page. Prompt for Login or Sign Up.</Text>
-      <Link style={styles.text} href="/signup">Sign Up</Link>
+      <Text style={styles.text}>Broom Service</Text>
+      <Text style={styles.text}>
+        This is the landing page. Prompt for Login or Sign Up.
+      </Text>
+      <Link style={styles.text} href="/signup">
+        Sign Up
+      </Link>
+      <Link style={styles.text} href="/login">
+        Login
+      </Link>
     </View>
   );
 }
@@ -13,12 +21,12 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#25292e',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#25292e",
+    alignItems: "center",
+    justifyContent: "center",
     gap: 10,
   },
   text: {
-    color: '#fff',
+    color: "#fff",
   },
 });
