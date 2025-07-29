cc0d10964b5fcff8a9fbfb36833784944c6d387c
Nabeel Sabzwari <nabeelsabz101@gmail.com>
2025-07-29 13:18:07 -0700

    Add login screen and modify signup route

--- broom_service_app/app/_layout.tsx
+++ broom_service_app/app/_layout.tsx
@@ -3,8 +3,9 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ title: 'Landing Page' }} />
-      <Stack.Screen name="sign_up" options={{ title: 'Sign Up Page' }} />
+      <Stack.Screen name="index" options={{ title: "Landing Page" }} />
+      <Stack.Screen name="signup" options={{ title: "Sign Up Page" }} />
+      <Stack.Screen name="login" options={{ title: "Login Page" }} />
     </Stack>
   );
 }
