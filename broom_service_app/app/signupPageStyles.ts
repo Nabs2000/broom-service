d06e774548e9f9722c211100f97b990065e0dd5d
Nabeel Sabzwari <nabeelsabz101@gmail.com>
2025-07-29 20:12:08 -0700

    Change login button name to signup button

--- broom_service_app/app/signupPageStyles.ts
+++ broom_service_app/app/signupPageStyles.ts
@@ -3,31 +3,31 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#25292e',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
+    backgroundColor: "#25292e",
+    alignItems: "center",
+    justifyContent: "flex-start",
   },
   backButton: {
-    position: 'absolute',
+    position: "absolute",
     top: 80,
     left: 20,
     padding: 10,
   },
   loginTitle: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 100,
     fontFamily: "Cochin",
     fontWeight: "bold",
     marginTop: 150,
     marginBottom: 70,
-  }, 
+  },
   userInputGroup: {
     width: 300,
-    alignItems: 'flex-start',
+    alignItems: "flex-start",
     marginBottom: 25,
   },
   fieldLabel: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 18,
     marginBottom: 5,
     fontWeight: "bold",
@@ -35,35 +35,24 @@ const styles = StyleSheet.create({
   userInput: {
     width: 300,
     height: 50,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     borderRadius: 10,
     paddingHorizontal: 16,
     fontSize: 20,
     marginBottom: 3,
   },
   loginButton: {
-    backgroundColor: '#4e9bde',
+    backgroundColor: "#4e9bde",
     paddingVertical: 16,
     paddingHorizontal: 40,
     borderRadius: 10,
-    alignItems: 'center',
+    alignItems: "center",
     marginTop: 10,
   },
-  loginButtonText: {
-    color: '#fff',
+  signupButtonText: {
+    color: "#fff",
     fontSize: 20,
-    fontWeight: 'bold',
-  },
-  forgotPassOrRegister: {
-    marginTop: 20,
-    alignItems: 'center',
-    gap: 10,
-  },
-  forgotPassOrRegisterText: {
-    color: '#4e9bde',
-    fontSize: 16,
-    textDecorationLine: 'underline',
-    marginTop: 5,
+    fontWeight: "bold",
   },
 });
 
