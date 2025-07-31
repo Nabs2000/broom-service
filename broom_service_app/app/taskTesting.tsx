619810ea09c468e4f3cd0a5fa1a748c494250fed
Nabeel Sabzwari <nabeelsabz101@gmail.com>
2025-07-31 15:09:51 -0700

    Test FlipCard

--- broom_service_app/app/taskTesting.tsx
+++ broom_service_app/app/taskTesting.tsx
@@ -1,15 +1,25 @@
-import styles from './styles/loginPageStyles';
-import { Keyboard, Text, TextInput, TouchableOpacity, TouchableWithoutFeedback, View } from 'react-native';
-import { Link, useRouter } from 'expo-router';
-import { AntDesign } from '@expo/vector-icons';
-import Task from './components/Task'
+import styles from "./styles/loginPageStyles";
+import {
+  Keyboard,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
+import { Link, useRouter } from "expo-router";
+import { AntDesign } from "@expo/vector-icons";
+import Task from "./components/Task";
 
 export default function TaskTesting() {
   const router = useRouter();
 
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
-      <Task/>
+      <View>
+        <Text>Hello!</Text>
+        <Task />
+      </View>
     </TouchableWithoutFeedback>
   );
 }
