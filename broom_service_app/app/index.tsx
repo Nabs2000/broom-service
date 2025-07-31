3caaf8cb25034ac9dd08f0052dc2d8f61fbbd8f6
Nabeel Sabzwari <nabeelsabz101@gmail.com>
2025-07-31 14:24:07 -0700

    Add link to task testing pg

--- broom_service_app/app/index.tsx
+++ broom_service_app/app/index.tsx
@@ -11,6 +11,9 @@ export default function Index() {
       <Link style={styles.text} href="/login">
         Login
       </Link>
+      <Link style={styles.text} href="/taskTesting">
+        Task Testing
+      </Link>
     </View>
   );
 }
