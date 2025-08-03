5f6492fcee5e8445e3dbf322195968dfbbe007f9
Nabeel Sabzwari <nabeelsabz101@gmail.com>
2025-08-02 20:28:22 -0700

    Add checkbox functionality for task completion

--- broom_service_app/app/components/Task.tsx
+++ broom_service_app/app/components/Task.tsx
@@ -1,9 +1,17 @@
 import React, { useState } from "react";
-import { Button, Text, View } from "react-native";
+import {
+  Button,
+  CheckBox,
+  Text,
+  View,
+  TouchableWithoutFeedback,
+} from "react-native";
 import FlipCard from "react-native-flip-card";
 import styles from "../styles/taskStyles";
 
 const Task = () => {
+  const [isSelected, setSelection] = useState(false);
+
   return (
     <FlipCard flipVertical={true} flipHorizontal={false}>
       {/* Face Side */}
@@ -11,6 +19,16 @@ const Task = () => {
         <Text style={styles.title}>Task Name</Text>
         <Text style={styles.text}>Assigned to: Nabeel Sabzwari</Text>
         <Text style={styles.text}>Due date: 07/12/2025</Text>
+        <TouchableWithoutFeedback onPress={(e) => e.stopPropagation()}>
+          <View style={styles.checkboxContainer}>
+            <CheckBox
+              value={isSelected}
+              onValueChange={setSelection}
+              style={styles.checkbox}
+            />
+            <Text style={styles.label}>Completed?</Text>
+          </View>
+        </TouchableWithoutFeedback>
       </View>
       {/* Back Side */}
       <View style={styles.view}>
