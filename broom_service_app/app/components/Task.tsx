09f3851c14aa982fbbe949874f36fa80d848909d
Nabeel Sabzwari <nabeelsabz101@gmail.com>
2025-08-03 15:15:45 -0700

    Use Animated instead of direct styling

--- broom_service_app/app/components/Task.tsx
+++ broom_service_app/app/components/Task.tsx
@@ -1,28 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import {
   Button,
-  CheckBox,
+  Animated,
   Text,
   View,
   TouchableWithoutFeedback,
+  useAnimatedValue,
 } from "react-native";
 import FlipCard from "react-native-flip-card";
+import BouncyCheckBox from "react-native-bouncy-checkbox";
 import styles from "../styles/taskStyles";
 
 const Task = () => {
   const [isSelected, setSelection] = useState(false);
+  const animation = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.timing(animation, {
+      toValue: isSelected ? 1 : 0,
+      duration: 500,
+      useNativeDriver: false,
+    }).start();
+  }, [isSelected, animation]);
+
+  const backgroundColor = animation.interpolate({
+    inputRange: [0, 1],
+    outputRange: ["#f08080", "#90ee90"],
+  });
 
   return (
     <FlipCard flipVertical={true} flipHorizontal={false}>
       {/* Face Side */}
-      <View
+      <Animated.View
         style={[
           styles.view,
           {
-            backgroundColor: isSelected ? "#90ee90" : "#f08080",
-            transitionProperty: "background-color",
-            transitionDuration: "500ms",
-            transitionTimingFunction: "ease",
+            backgroundColor: backgroundColor,
           },
         ]}
       >
@@ -31,15 +44,16 @@ const Task = () => {
         <Text style={styles.text}>Due date: 07/12/2025</Text>
         <TouchableWithoutFeedback onPress={(e) => e.stopPropagation()}>
           <View style={styles.checkboxContainer}>
-            <CheckBox
-              value={isSelected}
-              onValueChange={setSelection}
+            <BouncyCheckBox
+              size={15}
+              isChecked={isSelected}
+              onPress={setSelection}
               style={styles.checkbox}
             />
             <Text style={styles.label}>Completed?</Text>
           </View>
         </TouchableWithoutFeedback>
-      </View>
+      </Animated.View>
       {/* Back Side */}
       <View
         style={[
