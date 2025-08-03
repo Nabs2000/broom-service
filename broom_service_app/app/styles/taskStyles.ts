d0ea6b06fa39aef2eb3da4557cc208f05e0c91d4
Nabeel Sabzwari <nabeelsabz101@gmail.com>
2025-08-02 20:01:50 -0700

    Update task style sheet

---
+++ broom_service_app/app/styles/taskStyles.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  view: {
+    alignItems: "center",
+    justifyContent: "center",
+    height: 200,
+    backgroundColor: "#f0f0f0",
+    borderRadius: 12,
+    shadowColor: "#000",
+    shadowOpacity: 0.1,
+    shadowRadius: 8,
+    elevation: 4,
+  },
+  title: { fontSize: 20, fontWeight: "bold", color: "#333" },
+  text: { fontSize: 12, color: "#333" },
+  completeButton: { fontSize: 20, fontWeight: "bold", color: "#333" },
+});
+
+export default styles;
