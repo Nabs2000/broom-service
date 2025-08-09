6593701dc4df9b6a81fe6a8c6242d779d0ae229f
Nabeel Sabzwari <nabeelsabz101@gmail.com>
2025-08-09 16:59:04 -0700

    Add login form to home page

--- broom_service_app/app/index.tsx
+++ broom_service_app/app/index.tsx
@@ -1,17 +1,60 @@
-import { StyleSheet, Text, View } from "react-native";
-import { Link } from "expo-router";
+import {
+  Keyboard,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import { Link, useRouter } from "expo-router";
 import styles from "./styles/indexPageStyles";
+import { AntDesign } from "@expo/vector-icons";
 
 export default function Index() {
+  const router = useRouter();
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Broom Service</Text>
-      <Link style={styles.text} href="/signup">
-        Sign Up
-      </Link>
-      <Link style={styles.text} href="/login">
-        Login
-      </Link>
-    </View>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+      <View style={styles.container}>
+        <Text style={styles.titleText}>🧹 Broom Service 🧹</Text>
+        <Text style={styles.text}>A fun way to clean the house</Text>
+        {/* User Log in Fields */}
+        <View style={styles.userInputGroup}>
+          <Text style={styles.fieldLabel}>Username or Email</Text>
+          <TextInput
+            style={styles.userInput}
+            placeholder="example@gmail.com"
+            placeholderTextColor="#aaa"
+            autoCapitalize="none"
+            keyboardType="email-address"
+          />
+          <Text style={styles.fieldLabel}>Password</Text>
+          <TextInput
+            style={styles.userInput}
+            placeholder="*******"
+            placeholderTextColor="#aaa"
+            secureTextEntry
+          />
+        </View>
+
+        {/* Log In Button */}
+        <TouchableOpacity
+          style={styles.loginButton}
+          onPress={() => alert("Log in Button is Pressed.")}
+        >
+          <Text style={styles.loginButtonText}>Log In</Text>
+        </TouchableOpacity>
+
+        {/* Forgot Password or Register */}
+        <View style={styles.forgotPassOrRegister}>
+          <Link href="/forgotPass" style={styles.forgotPassOrRegisterText}>
+            Forgot Password? Reset
+          </Link>
+          <Link href="/signup" style={styles.forgotPassOrRegisterText}>
+            Don&apos;t have an account? Sign Up
+          </Link>
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
