cf379d816c7a1e8e25346ca11643e9cc4afd62d7
Nabeel Sabzwari <nabeelsabz101@gmail.com>
2025-08-09 16:23:52 -0700

    Move index pg styles to separate file

---
+++ broom_service_app/app/styles/indexPageStyles.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#25292e",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 10,
+  },
+  text: {
+    color: "#fff",
+  },
+});
+
+export default styles;
